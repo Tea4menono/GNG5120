9817d66a272c85f523d7d7759a4c7cc62b657607
helloloop <1204386811@qq.com>
2022-11-03 15:15:44 -0400

    feature: test

--- src/components/Personal.vue
+++ src/components/Personal.vue
@@ -1,11 +1,27 @@
 <script setup>
-import { Cell, CellGroup, Popup, Image as VanImage, Icon } from "vant";
+import {
+  Cell,
+  CellGroup,
+  Popup,
+  Image as VanImage,
+  Icon,
+  Rate,
+  Switch,
+  Toast,
+} from "vant";
 import { ref } from "vue";
 
 const account = ref(false);
 const paymentMethod = ref(false);
 const deliveryInfo = ref(false);
 const historyOrders = ref(false);
+const settings = ref(false);
+const contactUs = ref(false);
+const termsOfService = ref(false);
+
+const rateValue = ref(3);
+const notificationChecked = ref(true);
+const dataChecked = ref(true);
 
 const showAccount = () => {
   account.value = true;
@@ -18,6 +34,19 @@ const showDeliveryInfo = () => {
 };
 const showHistoryOrders = () => {
   historyOrders.value = true;
+};
+const showSettings = () => {
+  settings.value = true;
+};
+const showContactUs = () => {
+  contactUs.value = true;
+};
+const ShowTermsOfService = () => {
+  termsOfService.value = true;
+};
+
+const update = () => {
+  Toast.success("1.0.0 is the lastest version.");
 };
 </script>
 
@@ -37,10 +66,11 @@ const showHistoryOrders = () => {
         <cell title="Payment Method" is-link @click="showPaymentMethod" />
         <cell title="Delivery Info" is-link @click="showDeliveryInfo" />
         <cell title="History Orders" is-link @click="showHistoryOrders" />
-        <cell title="Settings" is-link />
-        <cell title="Contact Us" is-link />
-        <cell title="Terms of Service" is-link />
+        <cell title="Settings" is-link @click="showSettings" />
+        <cell title="Contact Us" is-link @click="showContactUs" />
+        <cell title="Terms of Service" is-link @click="ShowTermsOfService" />
         <cell title="Version" value="v1.0.0" />
+        <cell title="Update Application" is-link @click="update" />
       </cell-group>
     </div>
 
@@ -131,6 +161,62 @@ const showHistoryOrders = () => {
           </template>
         </cell>
       </cell-group>
+    </popup>
+
+    <popup
+      v-model:show="settings"
+      position="right"
+      :style="{
+        width: '80%',
+        height: '100%',
+        background: '#f7f8fa',
+        'padding-top': '30px',
+      }"
+    >
+      <cell-group inset>
+        <cell title="Rate">
+          <Rate v-model="rateValue" icon="like" void-icon="like-o" />
+        </cell>
+        <cell title="Notification Push Allowed">
+          <template #right-icon>
+            <Switch v-model="notificationChecked" size="24" />
+          </template>
+        </cell>
+
+        <cell title="Data Collection Allowed">
+          <template #right-icon>
+            <Switch v-model="dataChecked" size="24" />
+          </template>
+        </cell>
+      </cell-group>
+    </popup>
+
+    <popup
+      v-model:show="contactUs"
+      position="right"
+      :style="{
+        width: '80%',
+        height: '100%',
+        background: '#f7f8fa',
+        'padding-top': '30px',
+      }"
+    >
+      <cell-group inset>
+        <cell title="Mail" value="xxxx@gmail.com"> </cell>
+        <cell title="Phone" value="343434343"> </cell>
+      </cell-group>
+    </popup>
+
+    <popup
+      v-model:show="termsOfService"
+      position="right"
+      :style="{
+        width: '80%',
+        height: '100%',
+        background: '#f7f8fa',
+        'padding-top': '30px',
+      }"
+    >
     </popup>
   </div>
 </template>
