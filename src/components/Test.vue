c2c950d5cc19623dd26ca652d4a1d3fca1ec505f
helloloop <1204386811@qq.com>
2022-12-03 20:28:28 -0500

    ssss

---
+++ src/components/Test.vue
@@ -0,0 +1,92 @@
+<template>
+  <div ref="main" class="main"></div>
+
+  <div @click="save">123</div>
+</template>
+<script setup>
+import { onMounted, ref } from "vue";
+import html2pdf from "html2pdf.js";
+import * as echarts from "echarts";
+const main = ref(null);
+onMounted(() => {
+  var myChart = echarts.init(main.value);
+  // 绘制图表
+  myChart.setOption({
+    title: {},
+    tooltip: {},
+    xAxis: {
+      data: ["2018", "2019", "2020", "2021", "2022"],
+      name: "Year",
+      nameLocation: "center",
+      nameGap: 50,
+    },
+    yAxis: {
+      type: "value",
+      name: "The number of publications",
+      nameLocation: "center",
+      nameGap: 50,
+    },
+    aria: {
+      enabled: true,
+      decal: {
+        show: true,
+      },
+    },
+    series: [
+      {
+        name: "price",
+        type: "bar",
+        data: [
+          {
+            value: 1140,
+            itemStyle: {
+              color: "#c8f7c5",
+            },
+          },
+          {
+            value: 1880,
+            itemStyle: {
+              color: "#c3ff68",
+            },
+          },
+          {
+            value: 2630,
+            itemStyle: {
+              color: "#93faa5",
+            },
+          },
+          {
+            value: 3180,
+            itemStyle: {
+              color: "#b2de27",
+            },
+          },
+          {
+            value: 2609,
+            itemStyle: {
+              color: "#0db4b9",
+            },
+          },
+        ],
+        barWidth: 50,
+        label: {
+          show: true,
+          formatter: "{c}",
+          color: "black",
+          position: "top",
+        },
+      },
+    ],
+  });
+});
+
+const save = () => {
+  html2pdf(main.value);
+};
+</script>
+<style scoped lang="less">
+.main {
+  width: 1000px;
+  height: 1000px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/components/Home.vue";
 import Detail from "@/components/Detail.vue";
 import Checkout from "@/components/Checkout.vue";
+import Test from "@/components/Test.vue";
 
 const router = createRouter({
   history: createWebHistory(), // history 模式
@@ -36,6 +37,14 @@ const router = createRouter({
       component: Checkout,
       meta: {
         title: "checkout",
+      },
+    },
+    {
+      path: "/test",
+      name: "test",
+      component: Test,
+      meta: {
+        title: "test",
       },
     },
   ],
